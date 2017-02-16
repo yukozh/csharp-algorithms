5a9a4cca66cc4daefe9f34efe8d816021340bbc2
雨宮丶優子 <911574351@qq.com>
2017-02-16 11:55:29 +0800

    Dijkstra

---
+++ Dijkstra/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dijkstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new int[][]
+            {
+                new [] { 0, 50, 10, -1, 45, -1},
+                new [] { -1, 0, -1, -1,10,-1 },
+                new [] { 20, -1, 0, 15, -1, -1},
+                new [] { -1, 20, -1, 0, 35, -1},
+                new [] { -1, -1, -1, 30, 0, -1 },
+                new [] { -1, -1, -1, 3, -1, 0 }
+            };
+
+            foreach (var x in Dijkstra(g, 0))
+                if (x < 10000)
+                    Console.Write(x + " ");
+                else
+                    Console.Write("x ");
+            Console.Read();
+        }
+        
+        static IEnumerable<int> Dijkstra(int[][] g, int start)
+        {
+            // Arrange
+            var tmp = new DijkstraInfo[g.Length];
+            for (var i = 0; i < g.Length; i++)
+            {
+                tmp[i] = new DijkstraInfo();
+                tmp[i].Index = i;
+            }
+
+            // Init
+            var n = g.Length;
+            var pos = start;
+            for (var i = 0; i < g.Length; i++)
+            {
+                if (g[pos][i] >= 0 && tmp[i].Distance > g[pos][i])
+                {
+                    tmp[i].Distance = g[pos][i];
+                    tmp[i].Path = pos;
+                }
+            }
+            tmp[pos].IsVisited = true;
+
+            // Loop
+            do
+            {
+                var di = tmp.Where(x => !x.IsVisited && x.Distance < 10000).OrderBy(x => x.Distance).FirstOrDefault();
+                if (di == null)
+                    break;
+                pos = di.Index;
+                for (var i = 0; i < g.Length; i++)
+                {
+                    if (g[pos][i] >= 0 && tmp[i].Distance > g[pos][i] + tmp[pos].Distance)
+                    {
+                        tmp[i].Distance = g[pos][i] + tmp[pos].Distance;
+                        tmp[i].Path = pos;
+                    }
+                }
+                tmp[pos].IsVisited = true;
+            }
+            while (--n != 0);
+
+            // Output
+            foreach (var x in tmp)
+                yield return x.Distance;
+        }
+
+        class DijkstraInfo
+        {
+            public int Index { get; set; }
+            public int Distance { get; set; } = 10000;
+            public int Path { get; set; } = -1;
+            public bool IsVisited { get; set; } 
+        }
+    }
+}
