fbeefcb662cdf3b8b512aad371e1025faa21451b
雨宮丶優子 <911574351@qq.com>
2017-02-16 13:53:00 +0800

    Search tree

---
+++ SearchTree/Map.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchTree
+{
+    public class BitreeNode
+    {
+        public int Key { get; set; }
+
+        public string Value { get; set; }
+
+        public BitreeNode Left { get; set; }
+
+        public BitreeNode Right { get; set; }
+    }
+
+    public class Map
+    {
+        private BitreeNode root;
+
+        private BitreeNode Find(int key, BitreeNode n)
+        {
+            if (n == null)
+                return null;
+            if (n.Key == key)
+                return n;
+            if (n.Key < key)
+                return Find(key, n.Right);
+            else
+                return Find(key, n.Left);
+        }
+
+        private void Insert(int key, string value, BitreeNode n)
+        {
+            if (n.Key < key)
+            {
+                if (n.Right == null)
+                {
+                    n.Right = new BitreeNode
+                    {
+                        Key = key,
+                        Value = value
+                    };
+                }
+                else
+                {
+                    Insert(key, value, n.Right);
+                }
+            }
+            else
+            {
+                if (n.Left == null)
+                {
+                    n.Left = new BitreeNode
+                    {
+                        Key = key,
+                        Value = value
+                    };
+                }
+                else
+                {
+                    Insert(key, value, n.Left);
+                }
+            }
+        }
+
+        public string this[int key]
+        {
+            get
+            {
+                if (root == null)
+                    return null;
+                else
+                    return Find(key, root).Value;
+            }
+            set
+            {
+                if (root == null)
+                {
+                    root = new BitreeNode
+                    {
+                        Key = key,
+                        Value = value
+                    };
+                }
+                else
+                {
+                    var node = Find(key, root);
+                    if (node != null)
+                    {
+                        node.Value = value;
+                    }
+                    else
+                    {
+                        Insert(key, value, root);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SearchTree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrange
+            var map = new Map();
+            map[11] = "Hello World #11";
+            map[6] = "Hello World #6";
+            map[1] = "Hello World #1";
+            map[3] = "Hello World #3";
+            map[9] = "Hello World #9";
+            map[13] = "Hello World #13";
+            map[5] = "Hello World #5";
+            map[14] = "Hello World #14";
+            map[15] = "Hello World #15";
+            map[2] = "Hello World #2";
+
+            // Search
+            Console.WriteLine("Key: 3, Value: " + map[3]);
+            Console.Read();
+        }
+    }
+}
